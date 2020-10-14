8841bbcc003d23a2af084004e8efac1a6a3d7fa7
Audrey <audrey.cornaire@gmail.com>
2020-10-14 15:48:53 +0200

    [UPGRADE] main.py mise en place de l'api

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from typing import Optional
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
+import requests
 
 app = FastAPI()
-estimate = 'coucou'
+
+metre_carre = 740
+nb_pieces = 4
+terrain = 1000
+code_postal = 63000
+
+estimate = 'la fonction qui va faire l\'estimation'
+request={"metre_carre" : metre_carre, "nb_pieces" : nb_pieces, "terrain" : terrain, "code_postal" : code_postal}
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def welcome_message():
+    return {"welcome_message": "World"}
 
-@app.get("/estimate/")
-async def estimate(metre_carre: int = Form(...), nb_pieces: int = Form(...), terrain: int = Form(...), code_postal: int = Form(...)):
-    return {"estimate": estimate}
+@app.get("api/estimate/{request}")
+def estimate(metre_carre: int, nb_pieces: int, terrain: int, code_postal: int):
+    return {"estimate": estimate + nb_pieces}
+
+    # uvicorn main:app --host 0.0.0.0 --port 80
+
+url = "http://localhost:80/api/estimate/"
+response = requests.get(url, params = request)
+print(response.json())
