c550550f4ecbd0badad157090002f60be4e9b2fc
Audrey Cornaire <audrey.cornaire@gmail.com>
2021-09-22 16:28:17 +0200

    Update notebook clean datas

--- src/classes/split.py
+++ src/classes/split.py
@@ -4,20 +4,18 @@ import os
 
 class Splitter:
 
-    def split_datas(self, nomfichier, columnwanted, dirname):
+    def split_datas(self, filename, columnwanted, dirname):
         
         '''
         Break raw data into many files
         '''
-
-        filename = nomfichier
 
         os.makedirs(cr.CURATED_LOCAL_PATH + dirname + os.sep, exist_ok=True)
 
         csv.field_size_limit(10000000)
 
         with open(cr.CURATED_LOCAL_PATH + filename, encoding='utf-8') as file_stream:
-            file_stream_reader = csv.DictReader(file_stream, delimiter='|')
+            file_stream_reader = csv.DictReader(file_stream, delimiter=',')
             open_files_references = {}
 
             for row in file_stream_reader:
